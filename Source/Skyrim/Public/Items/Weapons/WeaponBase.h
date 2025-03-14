f736335345b972c1c8ca684b019eed150cbd5c09
DevKolIn <donghyeon.study@gmail.com>
2025-03-14 11:08:48 +0900

    [feat] Base Class 생성 - ItemBase - WeaponBase - ArmorBase - PotionBase

---
+++ Source/Skyrim/Public/Items/Weapons/WeaponBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Items/ItemBase.h"
+#include "WeaponBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYRIM_API AWeaponBase : public AItemBase
+{
+	GENERATED_BODY()
+	
+};
